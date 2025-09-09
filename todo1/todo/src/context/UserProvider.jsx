f7fa893c67bd8d0e3164ed1848f72c904b3256b7
N1ppexd <niilohanni@gmail.com>
2025-09-09 15:46:52 +0300

    Finally it works

--- todo1/todo/src/context/UserProvider.jsx
+++ todo1/todo/src/context/UserProvider.jsx
@@ -16,17 +16,17 @@ export default function UserProvider({children}) {
     const signUp = async () => {
 
         const headers = {headers: {'Content-Type': 'application/json'}}
-        await axios.post(`${apiUrl}/user/signup`, JSON.stringify({user: user}), headers)
+        await axios.post(`${import.meta.env.VITE_API_URL}/user/signup`, JSON.stringify({user: user}), headers)
         setUser({email: '', password: ''})
-    }
+    };
 
     const logIn = async () => {
-        
+
         const headers = {headers: {'Content-Type': 'application/json'}}
-        const response = await axios.post(`${apiUrl}/user/login`, JSON.stringify({user: user}), headers)
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/user/login`, JSON.stringify({user: user}), headers)
         setUser(response.data)
         sessionStorage.setItem('user', JSON.stringify(response.data))
-    }
+    };
 
     return (
         <UserContext.Provider value={{user, setUser, signUp, logIn}}>
